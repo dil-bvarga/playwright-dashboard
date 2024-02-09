a828bdd13c5336a74330345485de00edd79216e2
Balázs Varga <bvarga@diligent.com>
2024-02-09 15:10:52 +0100

    refactor: rename environment variable name

--- playwright-dashboard-api/src/services/playwright-test-result-bucket-browser-service.ts
+++ playwright-dashboard-api/src/services/playwright-test-result-bucket-browser-service.ts
@@ -55,28 +55,28 @@ export async function getBucketBrowserTestRunFolderNames(): Promise<string[]> {
  * This function fetches the XML response from the bucket browser API, converts it to JSON, 
  * and extracts the names of the test run result files. The bucket browser API URL prefix and 
  * result folder name are retrieved from the environment variables `BUCKET_BROWSER_API_URL_PREFIX` 
- * and `BUCKET_BROWSER_RESULT_FOLDER_NAME` respectively.
+ * and `BUCKET_BROWSER_CLIENT_RESULT_FOLDER_NAME` respectively.
  * 
  * @param {string} testSuiteRunFolderName - The name of the test suite run folder. Defaults to an empty string.
  * 
- * @throws {Error} Throws an error if the `BUCKET_BROWSER_API_URL_PREFIX` or `BUCKET_BROWSER_RESULT_FOLDER_NAME` 
+ * @throws {Error} Throws an error if the `BUCKET_BROWSER_API_URL_PREFIX` or `BUCKET_BROWSER_CLIENT_RESULT_FOLDER_NAME` 
  * environment variables are not set.
  * 
  * @returns {Promise<string[]>} A promise that resolves to an array of test run result file names, or rejects if an error occurs.
  */
 export async function getBucketBrowserTestRunResultFileNames(testSuiteRunFolderName: string = ''): Promise<string[]> {
     const bucketBrowserApiUrlPrefix: string = process.env.BUCKET_BROWSER_API_URL_PREFIX;
-    const bucketBrowserResultFolderName: string = process.env.BUCKET_BROWSER_RESULT_FOLDER_NAME;
+    const bucketBrowserClientResultFolderName: string = process.env.BUCKET_BROWSER_CLIENT_RESULT_FOLDER_NAME;
 
     if (!bucketBrowserApiUrlPrefix) {
         throw new Error('BUCKET_BROWSER_API_URL_PREFIX is not set');
-    } else if (!bucketBrowserResultFolderName) {
-        throw new Error('BUCKET_BROWSER_RESULT_FOLDER_NAME is not set');
+    } else if (!bucketBrowserClientResultFolderName) {
+        throw new Error('BUCKET_BROWSER_CLIENT_RESULT_FOLDER_NAME is not set');
     }
 
     let fileNames: string[] = [];
 
-    const bucketBrowserTestSuiteRunResultFolderApiUrl: string = `${bucketBrowserApiUrlPrefix}/${testSuiteRunFolderName}/${bucketBrowserResultFolderName}/&max-keys=50`;
+    const bucketBrowserTestSuiteRunResultFolderApiUrl: string = `${bucketBrowserApiUrlPrefix}/${testSuiteRunFolderName}/${bucketBrowserClientResultFolderName}/&max-keys=50`;
 
     const bucketBrowserTestSuiteRunResultFiles = await fetch(bucketBrowserTestSuiteRunResultFolderApiUrl, {
         method: 'GET',
@@ -113,12 +113,12 @@ export async function getBucketBrowserTestRunResultFileNames(testSuiteRunFolderN
  * This function fetches the JSON response from the bucket browser API for each file in the test suite run, 
  * and constructs a PlaywrightJSONReport object for each. The bucket browser client URL prefix and 
  * result folder name are retrieved from the environment variables `BUCKET_BROWSER_CLIENT_URL_PREFIX` 
- * and `BUCKET_BROWSER_RESULT_FOLDER_NAME` respectively.
+ * and `BUCKET_BROWSER_CLIENT_RESULT_FOLDER_NAME` respectively.
  * 
  * @param {string} testSuiteRunFolderName - The name of the test suite run folder. Defaults to an empty string.
  * @param {string[]} testSuiteRunFileNames - An array of test suite run file names. Defaults to an empty array.
  * 
- * @throws {Error} Throws an error if the `BUCKET_BROWSER_CLIENT_URL_PREFIX` or `BUCKET_BROWSER_RESULT_FOLDER_NAME` 
+ * @throws {Error} Throws an error if the `BUCKET_BROWSER_CLIENT_URL_PREFIX` or `BUCKET_BROWSER_CLIENT_RESULT_FOLDER_NAME` 
  * environment variables are not set.
  * 
  * @returns {Promise<PlaywrightJSONReport[]>} A promise that resolves to an array of PlaywrightJSONReport objects, 
@@ -126,18 +126,18 @@ export async function getBucketBrowserTestRunResultFileNames(testSuiteRunFolderN
  */
 export async function getBucketBrowserTestSuiteRunResults(testSuiteRunFolderName: string = '', testSuiteRunFileNames: string[] = []): Promise<PlaywrightJSONReport[]> {
     const bucketBrowserClientUrlPrefix: string = process.env.BUCKET_BROWSER_CLIENT_URL_PREFIX;
-    const bucketBrowserResultFolderName: string = process.env.BUCKET_BROWSER_RESULT_FOLDER_NAME;
+    const bucketBrowserClientResultFolderName: string = process.env.BUCKET_BROWSER_CLIENT_RESULT_FOLDER_NAME;
 
     if (!bucketBrowserClientUrlPrefix) {
         throw new Error('BUCKET_BROWSER_CLIENT_URL_PREFIX is not set');
-    } else if (!bucketBrowserResultFolderName) {
-        throw new Error('BUCKET_BROWSER_RESULT_FOLDER_NAME is not set');
+    } else if (!bucketBrowserClientResultFolderName) {
+        throw new Error('BUCKET_BROWSER_CLIENT_RESULT_FOLDER_NAME is not set');
     }
 
     let testResult: PlaywrightJSONReport[] = [];
 
     await Promise.all(testSuiteRunFileNames.map(async (fileName: string) => {
-        const bucketBrowserTestSuiteRunResultFileClientUrl: string = `${bucketBrowserClientUrlPrefix}/${testSuiteRunFolderName}/${bucketBrowserResultFolderName}/${fileName}`;
+        const bucketBrowserTestSuiteRunResultFileClientUrl: string = `${bucketBrowserClientUrlPrefix}/${testSuiteRunFolderName}/${bucketBrowserClientResultFolderName}/${fileName}`;
 
         const bucketBrowserTestResult: JSONReport = await fetch(bucketBrowserTestSuiteRunResultFileClientUrl, {
             method: 'GET',
