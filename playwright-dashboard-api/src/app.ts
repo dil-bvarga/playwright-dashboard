6540d1be9afd033d88c27188a554c816d1a97b30
Balázs Varga <bvarga@diligent.com>
2024-02-13 11:02:40 +0100

    fix: fix cors error

--- playwright-dashboard-api/src/app.ts
+++ playwright-dashboard-api/src/app.ts
@@ -4,9 +4,7 @@ import { testResultsRouter } from './routes/test-results.router';
 
 export const app: Express = express();
 
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/test-results', testResultsRouter);
