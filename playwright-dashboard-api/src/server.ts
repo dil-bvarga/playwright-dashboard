24635ba5e89e66835df09a88144c68ff28fa54cb
Balázs Varga <bvarga@diligent.com>
2024-02-13 15:26:04 +0100

    refactor: rename encironment variable and remove unused scripts

--- playwright-dashboard-api/src/server.ts
+++ playwright-dashboard-api/src/server.ts
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 async function startServer(): Promise<void> {
-    await connectToDatabase(process.env.MONGO_DB_LOCAL_URL);
+    await connectToDatabase(process.env.DATABASE_URL);
 
     server.listen(port, () => {
         console.log(`[server]: Server is running on port: ${port}`);
